ea4b25ae3543777d04191d75d80d1a144a77a49e
alalghzy <alalghzy@gmail.com>
2023-10-15 00:16:51 +0700

    tes

--- resources/views/layouts_admin.blade.php
+++ resources/views/layouts_admin.blade.php
@@ -86,7 +86,7 @@
                 <div class="sidebar-header position-relative">
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="logo">
-                            <a href="/admin"><img src="./assets/compiled/svg/logo.svg" alt="Logo"
+                            <a href="/"><img src="./assets/compiled/svg/logo.svg" alt="Logo"
                                     srcset=""></a>
                         </div>
                         <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
